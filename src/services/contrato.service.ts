38b354b715a65aef717b74f8f6b7f696f20753e6
Wilian Lopez <wlopezvilcarromero.hotmail.com>
2026-07-12 13:15:52 -0500

    refactor: limpiar componentes y servicios de contratos

--- src/services/contrato.service.ts
+++
@@ -1,7 +0,0 @@
-export * from '@/features/admin/contratos/services/contratos.api'
-export { contratosApi as contratoService } from '@/features/admin/contratos/services/contratos.api'
-export type {
-  GenerarContratoPayload,
-  ActualizarContratoPayload,
-  CambiarEstadoPayload,
-} from '@/features/admin/contratos/types'
